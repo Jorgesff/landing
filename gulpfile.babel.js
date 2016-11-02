c84131b880e3a0ef250e4d3dca803eb4261b540e
Jorge Souza Faleiros Filho <jfilho@localhost.localdomain>
2016-11-02 18:04:12 -0200

    commit inicial - 2 sect ok

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -109,6 +109,8 @@ gulp.task('scripts', () =>
       // Note: Since we are not using useref in the scripts build pipeline,
       //       you need to explicitly list your scripts here in the right order
       //       to be correctly concatenated
+      './app/scripts/jquery.js',
+      './app/scripts/bootstrap.js',
       './app/scripts/main.js'
       // Other scripts
     ])
